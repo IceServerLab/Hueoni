4e138f173238d8a675d1a4c12562f264a1bc6e92
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-08-02 11:05:33 +0900

    [Change] ゲーム開始コマンドを本格化

--- src/main/kotlin/jp/iceserver/hueoni/commands/StartCommand.kt
+++ src/main/kotlin/jp/iceserver/hueoni/commands/StartCommand.kt
@@ -1,15 +1,24 @@
 package jp.iceserver.hueoni.commands
 
 import hazae41.minecraft.kutils.bukkit.msg
+import jp.iceserver.hueoni.Hueoni
 import jp.iceserver.hueoni.config.MainConfig
 import jp.iceserver.hueoni.game.GameManager
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.TitlePart
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.Sound
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.scheduler.BukkitRunnable
 
 class StartCommand : CommandExecutor
 {
+    private var runnable: BukkitRunnable? = null
+
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean
     {
         sender as Player
@@ -20,7 +29,41 @@ class StartCommand : CommandExecutor
             return true
         }
 
-        GameManager.getInstance().startGame(sender.world)
+        if (runnable != null)
+        {
+            sender.msg("${MainConfig.prefix} &cゲーム開始カウントは既に始まっています。")
+            return true
+        }
+
+        if (GameManager.getInstance().runnable != null)
+        {
+            sender.msg("${MainConfig.prefix} &cゲームは既に始まっています。")
+            return true
+        }
+
+        runnable = object: BukkitRunnable()
+        {
+            var count = MainConfig.defaultCount
+
+            override fun run()
+            {
+                if (count == 0)
+                {
+                    this.cancel()
+                    GameManager.getInstance().startGame(sender.world)
+                    runnable = null
+                    return
+                }
+
+                Bukkit.getOnlinePlayers().forEach {
+                    it.sendTitlePart(TitlePart.TITLE, Component.text("${ChatColor.YELLOW}$count").asComponent())
+                    it.playSound(it.location, Sound.UI_BUTTON_CLICK, 40f, 1f)
+                }
+
+                count--
+            }
+        }
+        runnable?.runTaskTimer(Hueoni.plugin, 0L, 20L)
         return true
     }
 }
