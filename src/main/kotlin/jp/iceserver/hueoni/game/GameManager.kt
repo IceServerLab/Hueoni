2ad254f26e53e9ec521ed3807ba6aaa50ed51395
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-08-02 11:03:02 +0900

    [Add] 表示など諸々変更

--- src/main/kotlin/jp/iceserver/hueoni/game/GameManager.kt
+++ src/main/kotlin/jp/iceserver/hueoni/game/GameManager.kt
@@ -3,6 +3,8 @@ package jp.iceserver.hueoni.game
 import jp.iceserver.hueoni.Hueoni
 import jp.iceserver.hueoni.config.MainConfig
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.title.TitlePart
 import org.bukkit.*
 import org.bukkit.scheduler.BukkitRunnable
 import java.lang.StrictMath.floor
@@ -23,10 +25,15 @@ class GameManager
     }
 
     var gameTime: Int = MainConfig.defaultGameTime
+    var runnable: BukkitRunnable? = null
 
     fun startGame(world: World)
     {
         Bukkit.getOnlinePlayers().forEach {
+            it.sendTitlePart(TitlePart.TITLE, Component.text("${ChatColor.DARK_RED}${ChatColor.BOLD}ゲーム開始！！").asComponent())
+            it.sendTitlePart(TitlePart.SUBTITLE, Component.text("${ChatColor.GRAY}増え鬼").asComponent())
+            it.playSound(it.location, Sound.ENTITY_GENERIC_EXPLODE, 40f, 1f)
+
             if (listOf(GameMode.CREATIVE, GameMode.SPECTATOR).contains(it.gameMode)) return@forEach
 
             it.gameMode = GameMode.SURVIVAL
@@ -34,7 +41,7 @@ class GameManager
         }
         world.difficulty = Difficulty.NORMAL
 
-        object: BukkitRunnable()
+        runnable = object: BukkitRunnable()
         {
             var countdown: Int = gameTime
 
@@ -46,22 +53,42 @@ class GameManager
 
                     TODO("メッセージ")
 
-                    return
+                    runnable = null
                 }
 
                 Bukkit.getOnlinePlayers().forEach {
                     it.sendActionBar(
-                        Component.text(
-                        "${ChatColor.YELLOW}${ChatColor.BOLD}ゲーム時間 ${ChatColor.RESET}${convertTime(countdown).first}:${if (convertTime(countdown).second < 10) "0${convertTime(countdown).second}" else convertTime(countdown).second}"
-                    ))
+                        generateInfoBar(
+                            "${ChatColor.RED}${ChatColor.BOLD}鬼 ${ChatColor.RESET}${Bukkit.getScoreboardManager().mainScoreboard.getTeam("ONI")!!.size}人",
+                            "${ChatColor.YELLOW}${ChatColor.BOLD}ゲーム時間 ${ChatColor.RESET}${convertTime(countdown).first}:${if (convertTime(countdown).second < 10) "0${convertTime(countdown).second}" else convertTime(countdown).second}"
+                        )
+                    )
                 }
                 countdown--
             }
-        }.runTaskTimer(Hueoni.plugin, 0L, 20L)
+        }
+        runnable?.runTaskTimer(Hueoni.plugin, 0L, 20L)
     }
 
     fun forceFinishGame()
-    {}
+    {
+        if (runnable == null) return
+
+        runnable?.cancel()
+    }
 
     fun convertTime(time: Int): Pair<Int, Int> = Pair(floor(time / 60.0).toInt(), time - floor(time / 60.0).toInt() * 60)
+
+    private fun generateInfoBar(vararg info: String): TextComponent
+    {
+        val builder: StringBuilder = StringBuilder()
+
+        info.forEach {
+            builder.append(it)
+            builder.append("${ChatColor.GRAY} | ")
+        }
+
+        builder.setLength(builder.length - 4)
+        return Component.text(builder.toString())
+    }
 }
